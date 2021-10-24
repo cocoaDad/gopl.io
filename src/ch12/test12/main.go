aa5c0bd8a17d40b23d97400e67da7b299842983f
cocoaDad <dyfcocoa@outlook.com>
2021-10-24 16:39:45 +0800

    update

--- src/ch12/test12/main.go
+++ src/ch12/test12/main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main(){
+	fmt.Println("12.2--------------------------12.2--------------------------12.2")
 	fmt.Fprintf(os.Stdout, "%T\n", 3) // output the type of "3" directly
 	t := reflect.TypeOf(3)
 	fmt.Fprintf(os.Stdout, "%s\n", t.String()) // output the type with the help of function named reflect.Typeof()
@@ -14,12 +15,13 @@ func main(){
 	fmt.Println("----------------------------------------------------------------")
 	v := reflect.ValueOf(3)
 	fmt.Fprintf(os.Stdout, "%v\n", v) // output the reflect.Value
-	fmt.Fprintf(os.Stdout, "%v\n", v.String()) // attention: this output is "<int Value>"
+	fmt.Fprintf(os.Stdout, "%v\n", v.String()) //! attention: this output is "<int Value>"
 	t = v.Type() // output the type of the reflect.Value
 	fmt.Fprintf(os.Stdout, "%s\n", t.String())
 
 	x := v.Interface() // 将v转换为一个纯粹interface{}类型
 	i := x.(int)  // 接口断言，若x作为一个接口满足int这个接口的各个方法，则i的类型为int，值为3
+	v = reflect.ValueOf(x)
 	fmt.Fprintf(os.Stdout, "%d\n", i)
 	fmt.Println("----------------------------------------------------------------")
 	v = reflect.ValueOf("String")
@@ -27,4 +29,7 @@ func main(){
 	fmt.Fprintf(os.Stdout, "%v\n", v.String()) // !+ only when the type of the v is string, function with Stringer can output String
 	t = v.Type() // output the type of the reflect.Value
 	fmt.Fprintf(os.Stdout, "%s\n", t.String())
+	fmt.Println("12.3--------------------------12.3--------------------------12.3")
+
+
 }
